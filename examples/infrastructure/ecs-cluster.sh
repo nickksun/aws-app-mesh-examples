754af5ff40577550ac50446bc5cfa936b7cb7433
Sun <yaoys@784f43a2ea2b.ant.amazon.com>
2019-04-16 11:58:21 +1000

    fixed CFN template

--- examples/infrastructure/ecs-cluster.sh
+++ examples/infrastructure/ecs-cluster.sh
@@ -11,6 +11,4 @@ aws --profile "${AWS_PROFILE}" --region "${AWS_DEFAULT_REGION}" \
     --template-file "${DIR}/ecs-cluster.yaml"  \
     --parameter-overrides \
     EnvironmentName="${ENVIRONMENT_NAME}" \
-    KeyName="${KEY_PAIR_NAME}" \
-    ECSServicesDomain="${SERVICES_DOMAIN}" \
-    ClusterSize="${CLUSTER_SIZE:-5}"
+    ECSServicesDomain="${SERVICES_DOMAIN}"
