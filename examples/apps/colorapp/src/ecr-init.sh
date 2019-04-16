a56645294b871d4d127a98af9f18deb887889311
Sun <yaoys@784f43a2ea2b.ant.amazon.com>
2019-04-16 12:15:14 +1000

    fixed ecr-init script

--- examples/apps/colorapp/src/ecr-init.sh
+++ examples/apps/colorapp/src/ecr-init.sh
@@ -1,20 +1,12 @@
 #!/bin/sh -e
 
-TAG=$(git describe --abbrev=0 --tags)
-
 packages=(
     "colorteller"
     "gateway"
-    # "exporter"
     "prometheus"
 )
 namespace="$AWS_ACCOUNT.dkr.ecr.$AWS_REGION.amazonaws.com"
 
-
-i=0
-total=${#packages[@]}
 for pkg in ${packages[@]}; do
-    ((i++))
-    image=pkg
-    aws ecr create-repository --repository-name $image
+    aws ecr create-repository --repository-name $pkg
 done
